912d6d067aa468c636d746dbbadd5691ddc391e9
Ian Moses <imoses2@hotmail.com>
2024-05-27 18:43:25 -0700

    Created a WPF app for running the robot

---
+++ FlappyBirdRobot/Script.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace FlappyBirdRobot
+{
+    internal static class Script
+    {
+        public static void Run()
+        {
+            Trace.WriteLine("Run");
+        }
+    }
+}
