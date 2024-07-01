2a2aec0eca39a6f83841cdb70ec4f756fc7d26c1
Ian Moses <imoses2@hotmail.com>
2024-07-01 11:13:26 -0700

    Updates

--- parameters.js
+++ parameters.js
@@ -7,7 +7,7 @@ var hurtBoxRadius = radius * 0.5;
 // pipe stuff
 var pipeWidth = 120;
 var pipeSpeed = 4;
-var pipePeriod = 60;
+var pipePeriod = 80;
 var pipeMinGap = 140;
 var pipeMaxGap = 250;
 var pipeMinDistanceToEdge = 90;
